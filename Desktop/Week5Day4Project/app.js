9a49274bffc996befe4fc92ebfbdcea3881165de
Levi Smith <student@Mountainlands-Mac-mini.local>
2020-09-23 19:39:18 -0600

    Added Week5Day4Project

---
+++ Desktop/Week5Day4Project/app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color','white');
+
+$('#nav').css('background-color','#145');
+
+$('li:even').css('background-color','#ccc');
+
+$('li:even').css('background-color','#aaa');
+
+$('#myInput').val('Levi');
